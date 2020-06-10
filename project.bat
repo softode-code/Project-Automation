0e6862040fa6eaca6c1c3a4daeec82f8abe4f0a4
Softode <softode-code@users.noreply.github.com>
2020-06-10 14:55:40 +0500

    initial commit

---
+++ project.bat
@@ -0,0 +1,22 @@
+@echo off
+if "%1" == "-p" (
+    echo "Initializing Python Project"
+    cd /
+    d:
+    cd Python Scripts
+    md "%2"
+    cd "%2"
+    git init
+    code .
+) else (
+    if "%1" == "-f" (
+        echo "Initializing Flutter Project"
+        cd /
+        d:
+        cd Softode\Flutter
+        flutter create "%2"
+        cd "%2"
+        git init
+        code .
+    )
+)
